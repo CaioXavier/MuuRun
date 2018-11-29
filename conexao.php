49ae82ab3a61eb0e534c625e65b380d9c3d242a5
CaioXavier <caiofelipe.ox@hotmail.com>
2018-11-29 11:43:06 -0200

    tabela

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
 //VARIAVEIS COM CONFIGURAÇÕES DO BANCO DE DADOS
 	$host = "muurun.database.windows.net";
-	$database = "muurun";
+	$database = "dbo.tab-contato";
 	$usuario = "muurun";
 	$senha = "maravaca@2018";
 	
